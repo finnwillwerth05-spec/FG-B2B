16378ff1b2ad4f435ed098d7d12d86fed7824a58
Family guardian B2B <finn.willwerth05@gmail.com>
2026-04-30 22:08:57 -0400

    feat(ui): scaffold shadcn primitives and tailwind preset

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,10 @@
+export { Button, buttonVariants, type ButtonProps } from './components/button.js';
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/card.js';
+export { cn } from './lib/utils.js';
